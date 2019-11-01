36c508ec28ffa4e0dabbbd20e6b89ade6a0f191b
Alex Peterson <petersonab@google.com>
2019-11-01 05:28:06 +0000

    Add NEON autoregression for Chroma grains.
    Min speedup over C from spec: 2.2x
    Max speedup over C from spec: 4.3x
    
    PiperOrigin-RevId: 277867910
    Change-Id: I50d583fb0b4a0511f522be7e6bf70b21f8696c45

--- src/dsp/film_grain_impl.h
+++ src/dsp/film_grain_impl.h
@@ -39,7 +39,7 @@ namespace dsp {
 // So int8_t is big enough for bitdepth 8, whereas bitdepths 10 and 12 need
 // int16_t.
 template <int bitdepth>
-using GrainType =
+using GrainTypeForBpp =
     typename std::conditional<bitdepth == 8, int8_t, int16_t>::type;
 
 template <int bitdepth>
@@ -70,7 +70,7 @@ enum {
 template <int bitdepth>
 class FilmGrain {
  public:
-  using GrainType = GrainType<bitdepth>;
+  using GrainType = GrainTypeForBpp<bitdepth>;
 
   FilmGrain(const FilmGrainParams& params, bool is_monochrome,
             bool color_matrix_is_identity, int subsampling_x, int subsampling_y,
@@ -110,6 +110,15 @@ class FilmGrain {
       const GrainType* luma_grain, int subsampling_x, int subsampling_y,
       int chroma_width, int chroma_height, GrainType* u_grain,
       GrainType* v_grain);
+
+#if LIBGAV1_ENABLE_NEON
+  template <int auto_regression_coeff_lag>
+  static void ApplyAutoRegressiveFilterToChromaGrains_NEON(
+      const FilmGrainParams& params, int grain_min, int grain_max,
+      const GrainType* luma_grain, int subsampling_x, int subsampling_y,
+      int chroma_width, int chroma_height, GrainType* u_grain,
+      GrainType* v_grain);
+#endif
 
   static void InitializeScalingLookupTable(int num_points,
                                            const uint8_t point_value[],
